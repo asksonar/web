fe2f89c43e71e336590f0e77899b251940959090
Jonathan Chu <jmchuster@gmail.com>
2015-03-02 14:52:59 -0800

    show hover link to user times and popup corresponding video

--- app/assets/javascripts/summaries.js
+++ app/assets/javascripts/summaries.js
@@ -11,92 +11,46 @@ $(function(){
     var times = JSON.parse(thisEl.find('script').html());
     var graph = thisEl.find('.graph');
 
-    //var histogram = new Histogram(times);
-    //times = histogram.distributeData();
-
-    /*
-    times = [
-      { "time" : 10, "scenario_step_result_id" : 13 },
-      { "time" : 15, "scenario_step_result_id" : 15 },
-      { "time" : 30, "scenario_step_result_id" : 18 },
-      { "time" : 30, "scenario_step_result_id" : 17 },
-      { "time" : 35, "scenario_step_result_id" : 20 },
-      { "time" : 65, "scenario_step_result_id" : 25 },
-      { "time" : 90, "scenario_step_result_id" : 22 },
-      { "time" : 200, "scenario_step_result_id" : 30 }
-    ];
-
-    times = [
-      { "time" : "00:30", "user1": 1},
-      { "time" : "01:00", "user1": 1},
-      { "time" : "01:30", "user1": 3},
-      { "time" : "02:00", "user1": 1},
-      { "time" : "02:30", "user1": 2},
-      { "time" : "03:00"},
-      { "time" : "03:00+"}
-    ]
-    */
-
     var chart = AmCharts.makeChart(graph.attr('id'), {
       "type": "serial",
       "theme": "light",
       "creditsPosition": "top-right",
-      "hideBalloonTime": 0,
-
-      "categoryField": "time",
-
+      "hideBalloonTime": 4000,
+      "categoryField": "display",
       "graphs": [{
         "fillAlphas": .3,
         "type": "column",
-        "valueField": "user1",
+        "valueField": "count",
         "showHandOnHover": true,
-        "columnWidth": 1
-      }, {
-        "fillAlphas": .3,
-        "type": "column",
-        "valueField": "user2",
-        "showHandOnHover": true,
-        "columnWidth": 1
-      }, {
-        "fillAlphas": .3,
-        "type": "column",
-        "valueField": "user3",
-        "showHandOnHover": true,
-        "columnWidth": 1
+        "columnWidth": 1,
+        "balloonFunction": function(graphDataItem, graph) {
+          var details = graphDataItem.dataContext.details;
+          var display = '';
+          var time = 0;
+          var mins, seconds;
+          for(var i = 0; i < details.length; i++) {
+            time = details[i].time;
+            mins = Math.floor(time / 60);
+            seconds = time % 60;
+
+            display += "<a href='#' class='linkVideo feeling' data-scenario-step-id='" + details[i].id + "'>" + mins + ":" + ('00' + seconds).slice(-2) + "</a><br/>";
+          }
+          return display;
+        }
       }],
-      /*
-      "valueAxes": [{
-        "duration": "ss",
-        "durationUnits": {
-          "hh": ":",
-          "mm": ":",
-          "ss": ""
-        },
-        //"maximum": maximum, // will be needed to cap extremes
-        "minVerticalGap": 25
-      }],
-      "graphs" : [{
-        "valueField" : "time",
-        "type" : "column",
-        "fillAlphas" : .3,
-        "showHandOnHover": true,
-        "columnWidth": 1
-      }],
-      */
+      "balloon": {
+        "fixedPosition": true
+      },
       "dataProvider": times
     });
 
-    //chart.categoryAxis.labelsEnabled = false;
-    //chart.categoryAxis.gridThickness = 0;
-
     chart.addListener('clickGraphItem', function(event) {
-      var scenarioStepResultId = event.item.dataContext['id'];
-      alert(scenarioStepResultId);
+      var details = event.item.dataContext['details'];
     });
+
   });
 
-  $('.feeling').click(function(){
-    debugger;
+  $('.summary_step_container').on('click', '.feeling', function(event){
     var thisEl = $(this);
     var stepId = thisEl.attr('data-scenario-step-id');
     var timeSeconds = thisEl.attr('data-feeling-at-seconds');
@@ -110,9 +64,12 @@ $(function(){
       { type: "video/webm", src: "http://video-js.zencoder.com/oceans-clip.webm" },
       { type: "video/ogg", src: "http://video-js.zencoder.com/oceans-clip.ogv" }
     ]);
-    video.currentTime(timeSeconds);
+    if (timeSeconds) {
+      video.currentTime(timeSeconds);
+    }
     // remove the big play button
-    $('#example_video_1').addClass('vjs-has-started');
+    //$('#example_video_1').addClass('vjs-has-started');
+    return false;
   });
 
 });
--- app/models/summary_step.rb
+++ app/models/summary_step.rb
@@ -1,5 +1,7 @@
 class SummaryStep
 
+  MAX_TIME_BUCKET=180
+
   def initialize(scenario_step)
     @scenario_step = scenario_step
   end
@@ -20,6 +22,32 @@ class SummaryStep
     @scenario_step.scenario_step_results.map { |result|
       { id: result.id, time: result.completed_seconds }
     }
+  end
+
+  def result_time_bucket_hash
+    buckets = {}
+    (0..MAX_TIME_BUCKET).step(30) do |i|
+        min = i / 60
+        seconds = i % 60
+        buckets[i] = {};
+        buckets[i][:display] = min.to_s + ':' + seconds.to_s.rjust(2, '0')
+        buckets[i][:display] += '+' if i == MAX_TIME_BUCKET
+        buckets[i][:details] = []
+        buckets[i][:count] = 0
+    end
+
+    #buckets = {
+    #  '0:00':[], '0:30':[], '1:00':[], '1:30':[], '2:00':[], '2:30':[], '3:00+':[]
+    #}
+    @scenario_step.scenario_step_results.each do |result|
+      bucket_time = [result.completed_seconds, MAX_TIME_BUCKET].min
+      bucket_time = (bucket_time / 30) * 30
+
+      buckets[bucket_time][:details] += [{id: result.id, time: result.completed_seconds}]
+      buckets[bucket_time][:count] += 1
+    end
+
+    return buckets
   end
 
   def delighted_feelings
