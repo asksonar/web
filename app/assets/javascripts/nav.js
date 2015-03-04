0d70c1036de56caac75e5f91da21580dd1b984a0
Jonathan Chu <jmchuster@gmail.com>
2015-03-04 15:15:48 -0800

    hook up to neuboard

---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('#toggle-left').click(function(){
+    //$('#main-wrapper').toggleClass('sidebar-mini');
+    // need to add icons
+    // should also add styling to collapse the upper left site icon
+  });
+});
