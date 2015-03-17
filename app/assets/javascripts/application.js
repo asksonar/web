11d52186b15ad14df2e8a0984b7a4d6c1cd7c8da
Jonathan Chu <jmchuster@gmail.com>
2015-03-17 11:46:16 -0700

    turn off turbolinks fix left icons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require amcharts/all
@@ -24,5 +23,4 @@
 
 // insert new packages above this line
 //= require_tree .
-//= require turbolinks
 
