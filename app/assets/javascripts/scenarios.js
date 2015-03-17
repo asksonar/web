587aed2932897a7aef0a9702d9b3cff2bfe81c59
Jonathan Chu <jmchuster@gmail.com>
2015-03-17 12:58:14 -0700

    clean out unused code fix template setting

--- app/assets/javascripts/scenarios.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-  $('.scenarios .panel').click(function(){
-    window.location.href = $(this).attr('path');
-  });
-});
--- app/controllers/user_scenarios_controller.rb
+++
@@ -1,5 +0,0 @@
-class UserScenariosController < ApplicationController
-  def index
-    @user_scenarios = UserScenario.where(user_id: session[:user_id])
-  end
-end
