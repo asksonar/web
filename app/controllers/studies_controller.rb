71a8591c2693ad0fe1b07535b569a8807e3f9196
Jonathan Chu <jmchuster@gmail.com>
2015-04-21 05:01:17 -0700

    save email to panelist reuse panelist row if same email exists

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -4,8 +4,13 @@ class StudiesController < ApplicationController
   end
 
   def create
+    @panelist = Panelist.find_by(email: params[:study][:email])
+    if @panelist.nil?
+      @panelist = Panelist.create(email: params[:study][:email])
+    end
+
     @scenario = Scenario.find_by_hashid(params[:study][:scenario_hashid])
-    @user_scenario = ScenarioResult.create(scenario: @scenario)
+    @user_scenario = ScenarioResult.create(scenario: @scenario, panelist: @panelist)
     render json: {hashid: @user_scenario.hashid}
   end
 
