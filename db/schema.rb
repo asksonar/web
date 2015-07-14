ca0cfe61ad74c15893a4eb879c8b29fc64a9bf00
Jonathan Chu <jmchuster@gmail.com>
2015-07-14 00:37:27 -0700

    add additional metadata to ResultVideos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630103902) do
+ActiveRecord::Schema.define(version: 20150709231128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,9 @@ ActiveRecord::Schema.define(version: 20150630103902) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "scenario_result_id"
+    t.string   "uuid"
+    t.float    "offset_seconds"
+    t.float    "length_seconds"
   end
 
   create_table "scenario_results", force: :cascade do |t|
