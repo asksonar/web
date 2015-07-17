4c59d50fde02953858e819deca259f8cca6f1a0b
Jonathan Chu <jmchuster@gmail.com>
2015-07-17 08:06:11 -0700

    use step video hashid as the base of the url

--- app/models/step_video.rb
+++ app/models/step_video.rb
@@ -1,15 +1,14 @@
 class StepVideo < ActiveRecord::Base
-  VIDEO_BASE = Rails.configuration.properties['video_base_url']
-
   belongs_to :result_step
-  delegate :panelist, :to => :result_step, :allow_nil => true
+
+  VIDEO_BASE = Rails.configuration.properties['video_base_url']
 
   def src_array
     # return [ { type: "video/mp4", src: "http://vjs.zencdn.net/v/oceans.mp4" } ]
 
     [
-      { type: "video/mp4", src: "#{VIDEO_BASE}/#{scenario_result.hashid}/#{hashid}.mp4" },
-      { type: "video/webm", src: "#{VIDEO_BASE}/#{scenario_result.hashid}/#{hashid}.webm" }
+      { type: "video/mp4", src: "#{VIDEO_BASE}/#{hashid}/video.mp4" },
+      { type: "video/webm", src: "#{VIDEO_BASE}/#{hashid}/video.webm" }
       #{ type: "video/ogg", src: "/videos/video_" + data.id + ".ogv" }
     ]
   end
