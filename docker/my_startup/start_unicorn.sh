9e71253a8e6268cf7d2af2ffaf71b0e86241ac48
Jonathan Chu <jmchuster@gmail.com>
2015-07-27 04:12:10 -0700

    move from unicorn to thin

--- docker/my_startup/start_unicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /home/app/webapp
-#echo 'bundle exec unicorn -c config/unicorn.rb -D'
-#bundle exec unicorn -c config/unicorn.rb -D
-#bundle exec unicorn -c config/unicorn.rb
-#echo 'bundle exec thin -S /tmp/thin.web.0.sock start'
-#bundle exec thin -S /tmp/thin.web.0.sock start
-echo 'bundle exec rails server -p 5000 -b 0.0.0.0'
-bundle exec rails server -p 3000 -b 0.0.0.0
