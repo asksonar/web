dee8df178a34886148a1820bd16c82215613b9ca
Jonathan Chu <jmchuster@gmail.com>
2015-07-29 17:07:21 -0700

    generate new transcription for sample results

--- app/models/step_transcription.rb
+++ app/models/step_transcription.rb
@@ -3,4 +3,10 @@ class StepTranscription < ActiveRecord::Base
 
   HASHIDS_SALT = 'AVGutk*f43Sw'
 
+  def generate_new_sample_result(new_result_step)
+    new_step_transcription = self.dup
+    new_step_transcription.result_step = new_result_step
+    new_step_transcription.save
+  end
+
 end
