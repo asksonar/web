d848edffb21928ad18f509f33e9905826484daec
Jonathan Chu <jmchuster@gmail.com>
2015-07-30 15:24:17 -0700

    render vertical line with a position absolute element instead
    overflow:hidden on every panel gives us issues

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,12 +1,16 @@
 module BootstrapHelper
 
-  def bsh_split_panel(border_panel_class, &block)
+  def bsh_split_panel(border_panel_class)
+    col, size, column = border_panel_class.split('-')
+    sizes = ['xs', 'sm', 'md', 'lg']
+    visible_index = sizes.find_index(size)
+    hidden_sizes = sizes.take(visible_index).map { |size| "hidden-#{size}" }.join(' ')
+
     raw <<-HTML.chomp!
       <div class='split_panel'>
         <div class='row'>
-          <div class='#{border_panel_class} split_panel_border'></div>
+          <div class='split_panel_border #{border_panel_class} #{hidden_sizes}'></div>
         </div>
-        #{capture(&block) if block_given?}
       </div>
     HTML
   end
