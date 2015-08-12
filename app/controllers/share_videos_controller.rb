657ca19dcc5f93de8c70da49551816fade0ba1b9
Jonathan Chu <jmchuster@gmail.com>
2015-08-12 16:10:24 -0700

    store share/videos view as results view if it's you

--- app/controllers/share_videos_controller.rb
+++ app/controllers/share_videos_controller.rb
@@ -9,6 +9,8 @@ class ShareVideosController < ApplicationController
     created_by_id = @scenario.created_by.id
     if current_researcher.nil? or current_researcher.id != created_by_id
       Analytics.instance.share_video_viewed(@scenario.created_by, request.remote_ip, @scenario, @result_step)
+    else
+      Analytics.instance.result_video_viewed(current_researcher, @scenario, @result_step)
     end
   end
 end
