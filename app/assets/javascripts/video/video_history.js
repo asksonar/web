8d7764567085b4d0c8f47a459479616de0296330
Jonathan Chu <jmchuster@gmail.com>
2015-08-25 17:53:07 -0700

    add history module

---
+++ app/assets/javascripts/video/video_history.js
@@ -0,0 +1,15 @@
+VideoHistory = function() {
+
+}
+
+VideoHistory.prototype.loadVideo = function(videoHashId) {
+  if (location.href.indexOf('videos') < 0) {
+    var newUrl = URI(location.href).segment('videos').segment(videoHashId).addSearch("t", 0);
+    history.replaceState({}, '', newUrl);
+  }
+}
+
+VideoHistory.prototype.unloadVideo = function() {
+  var newUrl = '/' + URI(location.href).segment(0) + '/' + URI(location.href).segment(1);
+  history.replaceState({}, '', newUrl);
+}
