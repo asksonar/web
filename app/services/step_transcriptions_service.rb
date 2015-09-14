edf79b2820d93e392c01b5aef47c33536cd608d8
Jonathan Chu <jmchuster@gmail.com>
2015-09-14 06:58:03 -0700

    store original transcript when edited

--- app/services/step_transcriptions_service.rb
+++ app/services/step_transcriptions_service.rb
@@ -3,10 +3,11 @@ class StepTranscriptionsService
 
   def update_from_hashid(hashid:, offset_seconds:, text:)
     step_transcription = StepTranscription.find_by_hashid(hashid)
-    step_transcription.update(
-      offset_seconds: offset_seconds,
-      text: text
-    )
+
+    step_transcription.original_text ||= step_transcription.text
+    step_transcription.offset_seconds = offset_seconds
+    step_transcription.text = text
+    step_transcription.save
 
     step_transcription
   end
