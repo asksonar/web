a0da463db77cb700f8608d3e9c98a07f9809f5a8
Jonathan Chu <jmchuster@gmail.com>
2015-10-27 14:03:20 -0700

    pagination for results page

--- app/queries/scenarios_query.rb
+++ app/queries/scenarios_query.rb
@@ -6,7 +6,6 @@ class ScenariosQuery
       .where(status: Scenario.statuses[:drafts])
       .where(created_by: created_by)
       .order(updated_at: :desc)
-      .map(&:prezi)
   end
 
   def results(extra_where = {})
@@ -14,6 +13,12 @@ class ScenariosQuery
       .where(status: [Scenario.statuses[:live], Scenario.statuses[:completed]])
       .where(extra_where)
       .order(published_at: :desc)
-      .map(&:prezi)
+  end
+
+  # page index is 0-based
+  def results_paged(page, page_size, extra_where = {})
+    results(extra_where)
+      .limit(page_size)
+      .offset(page * page_size)
   end
 end
