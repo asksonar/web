aa839440ec3a18880aba6728afb2715d1e60a2e6
Stacy Huang <stacy.y.huang@gmail.com>
2015-10-29 11:24:18 -0700

    Remove methods for updating first transcription and feelings from services

--- app/services/step_feelings_service.rb
+++ app/services/step_feelings_service.rb
@@ -10,8 +10,6 @@ class StepFeelingsService
       result_step.step_feelings.create(feeling)
     end
 
-    result_steps_service.update_feeling_count(result_step)
-
     result_step.step_feelings
   end
 end
