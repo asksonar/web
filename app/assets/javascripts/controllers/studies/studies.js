88f3086b10b3269bf48f104a38c90d11e8651f2f
Jonathan Chu <jmchuster@gmail.com>
2015-11-13 16:55:34 -0800

    split out chrome extension module for shared use

--- app/assets/javascripts/controllers/studies/studies.js
+++ app/assets/javascripts/controllers/studies/studies.js
@@ -4,6 +4,7 @@ $(function(){
     return;
   }
 
+  window.extension = new ExtensionController(sonar.chrome_app_id);
   window.view = new StudiesView({
     'ctnInstallChrome': $('#ctn-install-chrome'),
     'ctnInstallExtension': $('#ctn-install-extension'),
@@ -11,7 +12,7 @@ $(function(){
     'ctnStartFeedback': $('#ctn-start-feedback'),
     'ctnRestartFeedback': $('#ctn-restart-feedback')
   });
-  window.controller = new StudiesController(view, sonar.chrome_app_id, {
+  window.controller = new StudiesController(extension, view, {
     'btnInstallExtension': $('#btn-install-extension'),
     'btnStartFeedback': $('#btn-start-feedback'),
     'form': $('form')
---
+++ app/assets/javascripts/extension/extension_controller.js
@@ -0,0 +1,99 @@
+function ExtensionController(appId) {
+  this.appId = appId;
+}
+
+ExtensionController.prototype.hasChrome = function() {
+  return window.chrome && chrome.webstore && chrome.webstore.install;
+}
+
+ExtensionController.prototype.checkForExtension = function() {
+  var deferred = $.Deferred();
+  var responseCallback = function(response) {
+    if (response === true) {
+      this.checkForExtensionUpdate();
+      deferred.resolve();
+    } else {
+      deferred.reject();
+    }
+  };
+
+  chrome.runtime.sendMessage(this.appId, 'isInstalledApp?', $.proxy(responseCallback, this));
+
+  return deferred;
+}
+
+ExtensionController.prototype.checkForExtensionUpdate = function() {
+  var deferred = $.Deferred();
+
+  var responseCallback = function(response) {
+    if (response !== true) {
+      notify.warn('The extension needs to be updated.  Please refresh the page and try again.');
+      deferred.reject();
+    } else {
+      deferred.resolve();
+    }
+  }
+  chrome.runtime.sendMessage(this.appId, 'update!', $.proxy(responseCallback, this));
+
+  return deferred;
+}
+
+ExtensionController.prototype.installExtension = function() {
+  var deferred = $.Deferred();
+
+  var successCallback = function() {
+    notify.info('Extension successfully activated.');
+    deferred.resolve();
+  };
+
+  var failureCallback = function() {
+    notify.warn('There was an error activating the extension.');
+    deferred.reject();
+  };
+
+  chrome.webstore.install("https://chrome.google.com/webstore/detail/" + this.appId,
+    $.proxy(successCallback, this), $.proxy(failureCallback, this));
+
+  return deferred;
+}
+
+ExtensionController.prototype.startFeedback = function(data) {
+  var deferred = $.Deferred();
+
+  var launchedAppResponse = function(response) {
+    if (response === true) {
+      deferred.resolve();
+    } else if (typeof response === "string") {
+      notify.warn(response);
+      deferred.reject();
+    } else {
+      notify.warn('There was an error launching the study.');
+      deferred.reject();
+    }
+  }
+
+  var ajaxDone = function(response) {
+    var launchAppParams = {};
+    launchAppParams['scenario'] = sonar.scenario;
+    launchAppParams['scenarioResultHashId'] = response.hashid;
+    launchAppParams['screen'] = {
+      availLeft: screen.availLeft,
+      availTop: screen.availTop,
+      availWidth: screen.availWidth,
+      availHeight: screen.availHeight
+    }
+
+    chrome.runtime.sendMessage(this.appId, {launchApp: launchAppParams}, $.proxy(launchedAppResponse, this));
+  }
+
+  $.ajax({
+    url: '/studies',
+    method: 'POST',
+    data: data,
+    dataType: 'json'
+  }).done($.proxy(ajaxDone, this)).fail(function(jqXHR) {
+    notify.error(jqXHR.responseText, 'There was an error starting your study.');
+  });
+
+  return deferred;
+}
