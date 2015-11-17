f473225b582531973c2c15b6c1abbda500c2cc1d
Stacy Huang <stacy.y.huang@gmail.com>
2015-11-17 12:13:05 -0800

    Display note timestamp instead of creation date in result panel

--- app/helpers/pretty_print_time_helper.rb
+++ app/helpers/pretty_print_time_helper.rb
@@ -2,6 +2,14 @@ module PrettyPrintTimeHelper
   # pretty print seconds
   def ppt_seconds(seconds)
     if seconds > 60 * 60
+      Time.at(seconds).strftime('%-H:%M:%S')
+    else
+      Time.at(seconds).strftime('%M:%S')
+    end
+  end
+
+  def ppt_duration(seconds)
+    if seconds > 60 * 60
       Time.at(seconds).strftime('%-Hh %-Mm %-Ss')
     elsif seconds > 60
       Time.at(seconds).strftime('%-Mm %-Ss')
