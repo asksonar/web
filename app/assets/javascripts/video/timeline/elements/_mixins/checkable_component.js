39ab7945615c4e574c40aac5af88f9e19c326179
Jonathan Chu <jmchuster@gmail.com>
2015-11-19 12:11:45 -0800

    simplify note save success/fail inheritance

--- app/assets/javascripts/video/timeline/elements/_mixins/checkable_component.js
+++ app/assets/javascripts/video/timeline/elements/_mixins/checkable_component.js
@@ -2,7 +2,7 @@ CheckableComponent = function() {
   this.checkable = true;
 
   var _saveSuccess = this.saveSuccess || $.noop;
-  this.saveSuccess = function(event, response) {
+  this.saveSuccess = function(customMsg) {
     _saveSuccess.apply(this, arguments);
     this.$el.find('.video-check-show').attr('name', 'scenario_highlight[' + this.displayClass + '][' + this.hashid + ']');
   };
