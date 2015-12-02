f71188acbff7897e24f174da2eb7a84e8d7825df
Jonathan Chu <jmchuster@gmail.com>
2015-12-02 15:43:27 -0800

    generate uuid for companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,11 @@ class Company < ActiveRecord::Base
   has_many :researchers, inverse_of: :company
   has_many :responders, inverse_of: :company
 
+  after_initialize :default_values, unless: :persisted?
+
   HASHIDS_SALT = '&8cCkHr#gHYk'
+
+  def default_values
+    self.uuid = SecureRandom.uuid
+  end
 end
