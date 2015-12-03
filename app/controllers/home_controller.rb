42123a6ccc065578cb26d64ccca0261a31db14fe
Jonathan Chu <jmchuster@gmail.com>
2015-12-02 17:48:26 -0800

    merge old v1 and new v2 navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    if current_researcher
+    if current_researcher && current_researcher.has_v2
+      redirect_to trends_path
+    elsif current_researcher && current_researcher.has_v1
       redirect_to results_path
     else
       redirect_to new_researcher_session_path
