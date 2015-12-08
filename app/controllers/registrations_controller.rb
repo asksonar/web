3df77e9f7e447168ca1af4247aac6c51c4014617
Stacy Huang <stacy.y.huang@gmail.com>
2015-12-08 13:27:06 -0800

    Only update params if they are not blank

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,10 +6,27 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update_resource(resource, params)
-    if params[:password].blank? && params[:password_confirmation].blank?
-      resource.update_without_password(params)
-    else
-     super
+    params.each do |key, value|
+      if value.blank?
+        params.delete(key)
+      end
     end
+
+    if !params[:password].blank? && !params[:password_confirmation].blank?
+      current_password = params.delete(:current_password)
+      result = if resource.valid_password?(current_password)
+        resource.update_attributes(params)
+      else
+        resource.assign_attributes(params)
+        resource.valid?
+        resource.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+        false
+      end
+    else
+      result = resource.update_attributes(params)
+    end
+
+    resource.clean_up_passwords
+    result
   end
 end
