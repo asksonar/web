93e57d8fe3c668e1dd1ad4be249965d360584ca8
Jonathan Chu <jmchuster@gmail.com>
2015-12-09 13:13:38 -0800

    synchronous script loading + reverting from CDN to S3

--- vendor/assets/javascripts/load.js
+++ vendor/assets/javascripts/load.js
@@ -1,23 +1,22 @@
 (function() {
-  var script = document.createElement('script');
-  script.src='https://code.jquery.com/jquery-1.11.3.min.js';
-  script.id='jquery-js';
-  document.body.appendChild(script);
+  var loadJS = function(path) {
+    var xhrObj = new XMLHttpRequest();
+    xhrObj.open('GET', path, false);
+    xhrObj.send(null);
+    eval(xhrObj.responseText);
+  };
 
-  var css = document.createElement('link');
-  css.rel='stylesheet';
-  css.href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
-  css.id='bootstrap-css';
-  document.head.appendChild(css);
+  var loadCSS = function(path) {
+    var css = document.createElement('link');
+    css.rel='stylesheet';
+    css.href=path;
+    css.id='bootstrap-css';
+    document.head.appendChild(css);
+  };
 
-  css = document.createElement('link');
-  css.rel='stylesheet';
-  css.href='https://assets.asksonar.com/survey.css';
-  css.id='survey-css';
-  document.head.appendChild(css);
+  loadCSS('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
+  loadCSS('https://s3-us-west-1.amazonaws.com/assets.asksonar.com/survey.css');
 
-  script = document.createElement('script');
-  script.src='https://assets.asksonar.com/widget.js';
-  script.id='widget-js';
-  document.body.appendChild(script);
+  loadJS('https://code.jquery.com/jquery-1.11.3.min.js');
+  loadJS('https://s3-us-west-1.amazonaws.com/assets.asksonar.com/widget.js');
 }());
