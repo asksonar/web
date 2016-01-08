158656efc3c4a67be322c6b46c0bebbb7a09097e
Jonathan Chu <jmchuster@gmail.com>
2016-01-08 15:07:55 -0800

    clean up html for settings form

--- app/assets/javascripts/controllers/settings/send.js
+++ app/assets/javascripts/controllers/settings/send.js
@@ -5,19 +5,19 @@ $(function(){
 
   $('#survey_settings_email_followup_enabled').on('click', function() {
     if ($(this).prop('checked')) {
-      $('.email_followup').removeClass('disabled');
+      $('.email-followup').removeClass('disabled');
       $('#survey_settings_email_followup').prop('disabled', false);
     } else {
-      $('.email_followup').addClass('disabled');
+      $('.email-followup').addClass('disabled');
       $('#survey_settings_email_followup').prop('disabled', true).val('');
     }
   });
 
   $("input[name='survey_settings[survey_type]']").on('click', function() {
     if ($(this).val() === 'inapp') {
-      $('.email_followup').removeClass('hidden');
+      $('.email-followup').removeClass('hidden');
     } else {
-      $('.email_followup').addClass('hidden');
+      $('.email-followup').addClass('hidden');
     }
   });
 });
