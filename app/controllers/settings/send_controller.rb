23b5ccdc3ea4e510978c8e2b9d97b7c7350fb57e
Jonathan Chu <jmchuster@gmail.com>
2016-01-13 12:13:31 -0800

    remove unneeded redirect

--- app/controllers/settings/send_controller.rb
+++ app/controllers/settings/send_controller.rb
@@ -12,13 +12,12 @@ module Settings
       company = current_user.company
       survey_settings = company.survey_settings
       service.update(survey_settings, survey_params)
+
       if survey_settings.valid?
         flash[:info] = 'Your changes have been updated.'
-        redirect_to action: :show
-      else
-        @survey_settings = survey_settings.prezi
-        render :show
       end
+      @survey_settings = survey_settings.prezi
+      render :show
     end
 
     private
