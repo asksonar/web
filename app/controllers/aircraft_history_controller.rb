cfbdf1d65db23c681e3bab35add2239dabfa70a2
Stacy Huang <stacy.y.huang@gmail.com>
2016-10-06 13:02:50 -0700

    Add service for API call

--- app/controllers/aircraft_history_controller.rb
+++ app/controllers/aircraft_history_controller.rb
@@ -4,8 +4,6 @@ class AircraftHistoryController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    airsonar = Airsonar.new()
-
     options = Hash.new { |h,k| h[k] = Hash.new(&h.default_proc) }
     options[:user] = current_user.email
     options[:user_comment] = params[:aircraft_history][:user_comment]
@@ -14,7 +12,7 @@ class AircraftHistoryController < ApplicationController
       options[:aircraft_history][key] = value
     end
 
-    airsonar.create_aircraft_history(
+    api.create_aircraft_history(
       params[:aircraft_history][:msn],
       params[:aircraft_history][:aircraft_model],
       options
@@ -35,8 +33,6 @@ class AircraftHistoryController < ApplicationController
   end
 
   def update
-    airsonar = Airsonar.new()
-
     options = Hash.new { |h,k| h[k] = Hash.new(&h.default_proc) }
     options[:user] = current_user.email
     options[:user_comment] = user_comment
@@ -44,11 +40,11 @@ class AircraftHistoryController < ApplicationController
     update_params.each do |key, value|
       options[:aircraft_history][key] = value
     end
-
-    airsonar.update_aircraft_history(
+    
+    api.update_aircraft_history(
       params[:msn],
       params[:aircraft_model],
-      params[:delivery_date],
+      delivery_date,
       options
     )
 
@@ -56,16 +52,14 @@ class AircraftHistoryController < ApplicationController
   end
 
   def destroy
-    airsonar = Airsonar.new()
-
     options = Hash.new { |h,k| h[k] = Hash.new(&h.default_proc) }
     options[:user] = current_user.email
     options[:user_comment] = user_comment
 
-    airsonar.delete_aircraft_history(
+    api.delete_aircraft_history(
       params[:msn],
       params[:aircraft_model],
-      params[:delivery_date],
+      delivery_date,
       options
     )
 
@@ -82,10 +76,17 @@ class AircraftHistoryController < ApplicationController
     end
   end
 
+  def delivery_date
+    params[:delivery_date] || ''
+  end
+
   def update_params
     params.fetch(:aircraft_history, {}).permit(
-      :delivery_date, :registration, :operator_name, :seats_configuration,
-      :engine_name, :aircraft_status
+      :registration, :operator_name, :seats_configuration, :engine_name, :aircraft_status
     )
+  end
+
+  def api
+    @api ||= ResolutionsService.instance
   end
 end
